bae04874fb4a845d0f671992e11d2706b5f8f272
Per Øyvind Karlsen <proyvind@moondrake.org>
2016-03-15 19:35:12 +0100

    move initialization of game variables to separate function and get rid of while loop from gameCountinue()

--- Minesweeper-Java/Minesweeper.java
+++ Minesweeper-Java/Minesweeper.java
@@ -8,50 +8,52 @@ public class Minesweeper {
 	private static MineField field;
 	private static Ranking rank;
 	private static int result;
+
 	public static void main(String[] args) {
-		rank=new Ranking();
+		rank = new Ranking();
 		mainMessage();
-		while(gameCountinue());
+		initGame();
+		while (gameCountinue())
+			;
 		System.out.println("\nThank you for playing :) Have a nice day!");
-	}	
-	private static boolean gameCountinue() {
+	}
+
+	private static void initGame() {
 		field = new MineField();
 		result = 0;
-		while (true) {
-
-			field.show();
-			System.out.print("\nPlease enter your move(row col): ");
-			in = new Scanner(System.in);
-			String input = in.nextLine();
-
-			if (evalInput(input))
-				return false;
-
-			if (field.legalMoveString(input)) {
-				if (++result == 35) {
-					System.out.println("Congratulations you WON the game!");
-					rank.recordName(result);
-					return true;
-				}
-				continue;
-			}else if (field.getBoom()) {
-				System.out.println("\nBooooooooooooooooooooooooooooom!You stepped on a mine!You survived " + result + " turns");
-				rank.recordName(result);
-				return true;
-			}
-
-		}
-
 	}
-	
+
+	private static boolean gameCountinue() {
+		field.show();
+		System.out.print("\nPlease enter your move(row col): ");
+		in = new Scanner(System.in);
+		String input = in.nextLine();
+
+		if (evalInput(input))
+			return false;
+
+		if (field.legalMoveString(input)) {
+			if (++result == 35) {
+				System.out.println("Congratulations you WON the game!");
+				rank.recordName(result);
+				initGame();
+			}
+		} else if (field.getBoom()) {
+			System.out.println(
+					"\nBooooooooooooooooooooooooooooom!You stepped on a mine!You survived " + result + " turns");
+			rank.recordName(result);
+		}
+		return true;
+	}
+
 	private static boolean evalInput(String input) {
 		if (input.equalsIgnoreCase("exit")) {
 			rank.recordName(result);
 			return true;
-		}
-		else if (input.equalsIgnoreCase("restart"))
+		} else if (input.equalsIgnoreCase("restart")) {
 			rank.recordName(result);
-		else if (input.equalsIgnoreCase("top"))
+			initGame();
+		} else if (input.equalsIgnoreCase("top"))
 			rank.show();
 		return false;
 	}
