b9d31dd98c909d62e7f1dcae9e6227b48048721f
Per Øyvind Karlsen <proyvind@moondrake.org>
2016-03-15 18:35:05 +0100

    increment result in compare statement

--- Minesweeper-Java/Minesweeper.java
+++ Minesweeper-Java/Minesweeper.java
@@ -27,8 +27,7 @@ public class Minesweeper {
 				return false;
 
 			if (field.legalMoveString(input)) {
-				result++;
-				if (result == 35) {
+				if (++result == 35) {
 					System.out.println("Congratulations you WON the game!");
 					rank.recordName(result);
 					return true;
